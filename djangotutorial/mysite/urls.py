f4f06628ce38ac8be59ac080f97d423f132a2db6
Yoshihiro Nagasako <nagasako.yoshihiro@gmail.com>
2025-08-19 15:50:54 +0900

    LANGUAGE_CODE で切り替える

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+urlpatterns += i18n_patterns(
+    path("", include("polls.urls")),
+)
